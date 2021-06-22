838b6f5f172821248659368cc62eaa4065e4995e
David <vapor2008@naver.com>
2021-06-22 20:45:02 +0900

    Section 4

---
+++ Sokovan/Assets/Scripts/ItemBox.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBox : MonoBehaviour
+{
+    public bool isOveraped = false;
+    private Renderer myRenderer;
+
+    private Color originalColor;
+    public Color touchColor;
+    
+    
+    void Start()
+    {
+        myRenderer = GetComponent<Renderer>();
+        originalColor = myRenderer.material.color;
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Endpoint")
+        {
+            isOveraped = true;
+            myRenderer.material.color = touchColor;
+            Debug.Log("충돌 했음");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Endpoint")
+        {
+            isOveraped = false;
+            myRenderer.material.color = originalColor;
+            Debug.Log("충돌 했음");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Endpoint")
+        {
+            isOveraped = true;
+            myRenderer.material.color = touchColor;
+        }
+    }
+}
---
+++ Sokovan/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody playerRigidbody;
+
+    public GameManager gameManager;
+    public float speed = 10f;
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        if (gameManager.isGameOver)
+        {
+            return;
+        }
+        
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ=Input.GetAxis("Vertical");
+
+        float fallSpeed = playerRigidbody.velocity.y;
+
+        Vector3 velocity = new Vector3(inputX, 0, inputZ);
+        velocity = velocity * speed;
+
+        velocity.y = fallSpeed;
+        playerRigidbody.velocity = velocity;
+    }
+}
